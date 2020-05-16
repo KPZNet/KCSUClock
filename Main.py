18c6036256ea46e45b91567431de0772afb6aa42
KPZNET <kpznet@outlook.com>
2020-05-16 12:27:29 -0700

    Colors

--- Main.py
+++ Main.py
@@ -1,42 +1,49 @@
 import sys
 from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtGui import QPalette
 from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
 from PyQt5.QtCore import QSize
 from PyQt5.QtCore import QTimer
 import time
 
-class HelloWindow(QMainWindow):
 
-    def handleTimer(self):
-        localtime = time.asctime(time.localtime(time.time()))
-        self.timeLab.setText(localtime)
+class HelloWindow ( QMainWindow ) :
 
-    def __init__(self):
-        QMainWindow.__init__(self, None, QtCore.Qt.WindowStaysOnTopHint)
+    def handleTimer(self) :
+        localtime = time.asctime ( time.localtime ( time.time () ) )
+        self.timeLab.setText ( localtime )
 
-        self.timeLab = QLabel(self)
+    def __init__(self) :
+        QMainWindow.__init__ ( self, None, QtCore.Qt.WindowStaysOnTopHint )
+
+        # self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+
+        self.timeLab = QLabel ( self )
         # self.setMinimumSize(QSize(640, 480))
-        self.setWindowTitle("CSU Global")
+        self.setWindowTitle ( "CSU Global" )
 
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
+        central_widget = QWidget ( self )
+        self.setCentralWidget ( central_widget )
 
-        grid_layout = QGridLayout(self)
-        central_widget.setLayout(grid_layout)
+        grid_layout = QGridLayout ( self )
+        central_widget.setLayout ( grid_layout )
 
-        localtime = time.asctime(time.localtime(time.time()))
+        localtime = time.asctime ( time.localtime ( time.time () ) )
 
-        self.timeLab = QLabel('Pending Time', self)
-        self.timeLab.setAlignment(QtCore.Qt.AlignCenter)
-        grid_layout.addWidget(self.timeLab, 0, 0)
+        self.timeLab = QLabel ( 'Pending Time', self )
+        self.timeLab.setAlignment ( QtCore.Qt.AlignCenter )
+        grid_layout.addWidget ( self.timeLab, 0, 0 )
 
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.handleTimer)
-        self.timer.start (1000)
+        self.timeLab.setStyleSheet ( "background-color: yellow;border: 1px solid" )
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    mainWin = HelloWindow()
+        self.timer = QTimer ()
+        self.timer.timeout.connect ( self.handleTimer )
+        self.timer.start ( 1000 )
 
-    mainWin.show()
-    sys.exit( app.exec_() )
+
+if __name__ == "__main__" :
+    app = QtWidgets.QApplication ( sys.argv )
+    mainWin = HelloWindow ()
+
+    mainWin.show ()
+    sys.exit ( app.exec_ () )
