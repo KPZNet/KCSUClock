f7c637d8dc1db2d251a99c31c7daeb962cafb87c
KPZNET <kpznet@outlook.com>
2020-05-17 09:30:18 -0700

    Sizes, refactors

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtGui import QPalette
 from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget
 from PyQt5.QtCore import QSize
@@ -16,9 +16,6 @@ class HelloWindow ( QMainWindow ) :
     def __init__(self) :
         QMainWindow.__init__ ( self, None, QtCore.Qt.WindowStaysOnTopHint )
 
-        # self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
-
-        self.timeLab = QLabel ( self )
         # self.setMinimumSize(QSize(640, 480))
         self.setWindowTitle ( "CSU Global" )
 
@@ -28,22 +25,41 @@ class HelloWindow ( QMainWindow ) :
         grid_layout = QGridLayout ( self )
         central_widget.setLayout ( grid_layout )
 
-        localtime = time.asctime ( time.localtime ( time.time () ) )
+        self.CreateTimeDisplay ( grid_layout )
 
+        self.SetStyles ()
+        self.SetSize ()
+        self.StartTimeUpdates ()
+
+    def CreateTimeDisplay(self, grid_layout) :
         self.timeLab = QLabel ( 'Pending Time', self )
         self.timeLab.setAlignment ( QtCore.Qt.AlignCenter )
         grid_layout.addWidget ( self.timeLab, 0, 0 )
 
+    def SetStyles(self) :
         self.timeLab.setStyleSheet ( "background-color: yellow;border: 1px solid" )
 
+    def SetSize(self) :
+        localtime = time.asctime ( time.localtime ( time.time () ) )
+        metrics = QtGui.QFontMetrics ( self.timeLab.font () )
+        twidth = metrics.boundingRect ( localtime ).width ()
+        theight = metrics.boundingRect ( localtime ).height ()
+
+        self.setMinimumWidth ( twidth )
+        self.setMaximumHeight ( theight )
+
+    def StartTimeUpdates(self) :
         self.timer = QTimer ()
         self.timer.timeout.connect ( self.handleTimer )
         self.timer.start ( 1000 )
 
+    def StopTimeUpdates(self) :
+        self.timer.stop ()
 
 if __name__ == "__main__" :
     app = QtWidgets.QApplication ( sys.argv )
     mainWin = HelloWindow ()
 
     mainWin.show ()
+
     sys.exit ( app.exec_ () )
